cdc8fc2ca4bf97fc9b4a9e87d7a5630246e58334
codie <purvaath5@gmail.com>
2025-02-03 12:18:26 +0530

    Update 12.ReverseString.cpp

--- 12.ReverseString.cpp
+++ 12.ReverseString.cpp
@@ -1,12 +1,22 @@
-#include<iostream>
-#include <algorithm> 
+#include <iostream>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-   string str ="ABeDrewskOP";
-   reverse(str.begin(),str.end());
-   cout<<str<<endl;
+int main() {
+    string str = "Vikku";
+    reverse(str.begin(), str.end());  // Reverse str
+    cout << "Reversed str: " << str << endl;
+
+    string s = "kpiko";
+    int n = s.size();  // Use size of s, not str
+
+    for (int i = 0; i < n / 2; i++) {
+        swap(s[i], s[n - i - 1]);
+    }
+
+    cout << "Reversed s: " << s << endl;
 
     return 0;
 }
